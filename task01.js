827848b96765c23942bbfcdcdcefa9e46f24594a
Asheer-abbasi01 <abbasihashirabbasi0@gmail.com>
2024-07-25 11:25:08 +0000

    conversion of datatypes

--- task01.js
+++ task01.js
@@ -1,5 +1,9 @@
 let score = 33
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInStr = String(score)
-console.log(typeof valueInStr)
+//console.log(typeof valueInStr)
+
+let a = 33
+let stringNumber = String(a)
+console.log(typeof stringNumber)
